a8a0d7f383cc41c6fa163c25de0e4081e60cf0ef
seongmin <seongmin1117@gmail.com>
2025-04-07 13:22:42 +0900

    Feat: 공통 설정 활성화를 위한 어노테이션 추가

---
+++ src/main/java/com/genieus/common/annotation/EnableCommonLib.java
@@ -0,0 +1,15 @@
+package com.genieus.common.annotation;
+
+import com.genieus.common.config.CommonConfig;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(CommonConfig.class)
+public @interface EnableCommonLib {
+}
