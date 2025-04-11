d970454a99a5796cfe60e315498aa9173004f908
seongmin <seongmin1117@gmail.com>
2025-04-12 00:11:06 +0900

    fix: 결제생성 Dto 수정

--- src/main/java/com/genieus/common/internal/request/CreatePaymentRequest.java
+++ src/main/java/com/genieus/common/internal/request/CreatePaymentRequest.java
@@ -1,4 +1,3 @@
 package com.genieus.common.internal.request;
 
-public record CreatePaymentRequest(
-    Long userId, Long orderId, Long pgId, Integer price, String paymentMethod) {}
+public record CreatePaymentRequest(Long orderId, Integer amount) {}
