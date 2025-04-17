17f7838289385dff8f9d31a2b88011ab6da80ad0
seongmin <seongmin1117@gmail.com>
2025-04-17 23:45:11 +0900

    fix: fallback 예외 메시지 수정

--- src/main/java/com/genieus/common/event/util/EventDispatcher.java
+++ src/main/java/com/genieus/common/event/util/EventDispatcher.java
@@ -56,7 +56,7 @@ public class EventDispatcher {
       handlerMethod.method().invoke(handlerMethod.bean(), event);
     } catch (Exception e) {
       // 예외는 route() 단에서 잡히고 fallback 호출로 이어짐
-      throw new RuntimeException("이벤트 로직 처리에 실패하였습니다.", e);
+      throw new RuntimeException(e);
     }
   }
 
